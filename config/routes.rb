71ba0919e32c5bd237b97cc0e7cc73129171c7ac
kitagawahiroshi <puyon41@gmail.com>
2019-02-09 14:20:30 +0900

    create item model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+#CRUDアプリ(C:CREATE、R:READ、U:UPDATE、D:DELETE)
 Rails.application.routes.draw do
+  resources :items #いろいろまとめたコード $ rails routes とか $ bin/rails routes | grep items  http://localhost:3000/rails/info/routesで中身がみれる
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
