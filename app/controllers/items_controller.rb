ade11296405add7bd57bd2eace96863c5e4789f1
kitagawahiroshi <puyon41@gmail.com>
2019-02-09 15:50:14 +0900

    link list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
+  
   def show
     @item = Item.find(params[:id]) #@itemビューに情報をわたす AppのModelsの中のitemクラスのfindメソッドをつかう
   end 
+  
+  def index
+    @items = Item.all #itemではなくitems
+  end
+
 end
