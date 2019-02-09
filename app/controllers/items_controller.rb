cc442f14f327e0334c6fd1ba7549bb833f274e29
kitagawahiroshi <puyon41@gmail.com>
2019-02-09 16:37:37 +0900

    Registration function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,20 @@ class ItemsController < ApplicationController
     @items = Item.all #itemではなくitems
   end
 
+  def new
+    @item = Item.new #あたらしいItemをつくってビューに渡す
+  end
+
+  def create #viewはnew.html.erb
+    @item = Item.new(item_params) #paramsとはitemに関する情報のハッシュ？
+    @item.save #セーブする
+    redirect_to @item #商品ページに飛ばす
+  end
+
+  private #プライベート
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url) #ストロングパラメータ
+  end
+
 end
